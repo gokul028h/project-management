cd8a4a22d21b8af19ad97485c761f2cf001e80ca
Gokul-raj-P-2006 <gokulraj28022006@gmail.com>
2025-09-02 00:52:29 +0530

    redirect images to s3

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "pm-s3-images.s3.us-east-2.amazonaws.com",
+        hostname: "pm2-s3-imagess.s3.us-east-1.amazonaws.com",
         port: "",
         pathname: "/**",
       }
